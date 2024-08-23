7099d9053dfdc5c742ac2ccea1f1dbcf41841181
Alvan Samudio <alvan.samudio@utp.ac.pa>
2024-08-23 11:21:01 -0500

    Ejercicio de perfil de usuario completado

---
+++ TALLERES/TALLER_2/perfil_usuario.php
@@ -0,0 +1,10 @@
+<?php
+$nombre_completo="Alvan Martin Samudio";
+$edad= 26;
+$correo ="alvan.samudio@utp.ac.pa";
+$telefono="63846144";
+define("OCUPACION", "Estudiante");
+echo ("Hola, mi nombre es $nombre_completo, mi edad es: $edad, mi correo es: $correo, mi telefono es: $telefono<br><br>");
+print"Tengo $edad años y me llamo $nombre_completo<br><br>";
+printf("Me llamo %s y tengo %d años, mi correo es: %s, mi teléfono es: %s<br><br>", $nombre_completo,$edad,$correo,$telefono);
+?>
