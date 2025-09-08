b5e0507127dccd75b00f772db335456371e46eea
Alvan Samudio <marthin_0712@hotmail.com>
2025-09-08 18:48:29 -0500

    subiendo archivo para crear carpeta en el repo

---
+++ PARCIALES/PARCIAL_1/procesador_frases.php
@@ -0,0 +1,30 @@
+<?php
+include "operaciones_cadenas.php";
+
+$array_frases = ["Hola mundo Hola mundo ya viene navidad","Santa claus quiero quiero para esta navidad unos audifonos sony sony",
+ "Tienes privilegios de administrador","Son necesarias si el bloque de código tiene más de una línea. Para una sola línea, son opcionales pero recomendadas para claridad."];
+
+
+?>
+
+<!DOCTYPE html>
+
+<body>
+    <table>
+        <th>
+            Frases
+        </th>
+        <th>
+            Palabras Repetidas
+        </th>
+        <th>Capitalizar Palabras</th>
+    <?php foreach ($array_frases as $frases): ?>
+        <tr>
+        <td> <?php echo $frases?></td>
+        <td><?php echo contar_palabbras_repetidas($frases);?></td>
+        <td><?php echo capitalizar_palabras($frases); ?></td>
+        </tr>
+        <?php endforeach;?>
+    </table>
+</body>
+</html>
